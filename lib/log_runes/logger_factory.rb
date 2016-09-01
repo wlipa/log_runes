51a33bc9e3a81c620ff9dfe6a63f3e4c12f95d39
wlipa <dojo@masterleep.com>
2016-09-01 16:39:33 -0700

    use an ActiveSupport::Logger to provide silence call

--- lib/log_runes/logger_factory.rb
+++ lib/log_runes/logger_factory.rb
@@ -5,14 +5,14 @@ class LoggerFactory
 
     if Rails.env.development? || Rails.env.test?
       # Use a stdout logger to avoid piling up a mostly useless giant log file
-      stdout_logger = Logger.new(STDOUT)
+      stdout_logger = ActiveSupport::Logger.new(STDOUT)
       config.logger = ActiveSupport::TaggedLogging.new(stdout_logger)
       return
     end
     
     log_base = opts[:dir] || "#{Rails.root}/log"
     log_name = opts[:name] || Rails.env
-    l = Logger.new("#{log_base}/#{log_name}.log", 'daily')
+    l = ActiveSupport::Logger.new("#{log_base}/#{log_name}.log", 'daily')
 
     config.logger = opts[:not_tagged] ? l : ActiveSupport::TaggedLogging.new(l)
 
